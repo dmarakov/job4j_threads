53c6be1ffdf6b80246944d2e36e61d1be6ec1b26
Dmitry Marakov <dmarakov@devexperts.com>
2023-12-18 01:58:22 +0400

    4. Режим ожидания. [#231217 #464963]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -5,11 +5,9 @@ public class Wget {
         Thread thread = new Thread(
                 () -> {
                     try {
-                        int index = 0;
-                        while (index <= 100) {
-                            System.out.print("\rLoading : " + index + "%");
+                        for (int i = 0; i <= 100; i++) {
+                            System.out.print("\rLoading : " + i + "%");
                             Thread.sleep(1000);
-                            index++;
                         }
                     } catch (InterruptedException e) {
                         e.printStackTrace();
