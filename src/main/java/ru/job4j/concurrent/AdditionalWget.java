0ef9839310171573e60509b862b05166909e3d77
Dmitry Marakov <dmarakov@devexperts.com>
2023-12-20 03:36:24 +0400

    8. Скачивание файла с ограничением. [#144271 #467144]

--- src/main/java/ru/job4j/concurrent/AdditionalWget.java
+++ src/main/java/ru/job4j/concurrent/AdditionalWget.java
@@ -1,59 +1,63 @@
 package ru.job4j.concurrent;
 
-
-import java.io.File;
 import java.io.FileOutputStream;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.io.IOException;
 
-
 public class AdditionalWget implements Runnable {
     private final String url;
     private final int speed;
+    private final String file;
 
-    public AdditionalWget(String url, int speed) {
+    public AdditionalWget(String url, int speed, String file) {
         this.url = url;
         this.speed = speed;
+        this.file = file;
     }
 
     @Override
     public void run() {
         var startAt = System.currentTimeMillis();
-        var file = new File("tmp.xml");
         try (var input = new URL(url).openStream();
              var output = new FileOutputStream(file)) {
             System.out.println("Open connection: " + (System.currentTimeMillis() - startAt) + " ms");
             var dataBuffer = new byte[512];
             int bytesRead;
+            long bytesCount = 0;
+            long downloadAt = System.currentTimeMillis();
             while ((bytesRead = input.read(dataBuffer, 0, dataBuffer.length)) != -1) {
-                var downloadAt = System.nanoTime();
                 output.write(dataBuffer, 0, bytesRead);
-                long downloadSpeed = Math.round((dataBuffer.length * 1.0 / (System.nanoTime() - downloadAt)) * 1000000);
-                if (downloadSpeed > speed) {
-                    Thread.sleep(Math.round(downloadSpeed * 1.0 / speed));
+                bytesCount += bytesRead;
+                if (bytesCount >= speed) {
+                    long timeDiff = System.currentTimeMillis() - downloadAt;
+                    if (1000 > timeDiff) {
+                        Thread.sleep(1000 - timeDiff);
+                    }
                 }
+                bytesCount = 0;
+                downloadAt = System.currentTimeMillis();
             }
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-
     }
 
     public static void main(String[] args) throws InterruptedException {
-        if (args.length != 2) {
-            throw new IllegalArgumentException(String.format("2 parameters are required: url and "
-                            + "the number of bytes per millisecond separated by a space. You entered %d parameter(s)",
+        if (args.length != 3) {
+            throw new IllegalArgumentException(String.format("3 parameters are required: url, "
+                            + "the number of bytes per second and filename separated by a space. You entered %d parameter(s)",
                     args.length));
         }
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
+        String file = args[2];
         isValidURL(url);
         isValidSpeed(speed);
-        Thread wget = new Thread(new AdditionalWget(url, speed));
+        Thread wget = new Thread(new AdditionalWget(url, speed, file));
         wget.start();
         wget.join();
     }
