51b2c0df4829f1e943e36181d7e04f463ed1392a
Dmitry Marakov <dmarakov@devexperts.com>
2024-01-18 02:19:43 +0400

    1. Visibility. Общий ресурс вне критической секции [#1102 #473594]

--- src/main/java/ru/job4j/concurrent/ParseFile.java
+++ src/main/java/ru/job4j/concurrent/ParseFile.java
@@ -10,11 +10,19 @@ public class ParseFile {
         this.file = file;
     }
 
+    public String getContentWithoutUnicode() throws IOException {
+        return getContent(it -> true);
+    }
+
+    public String getContentWithUnicode() throws IOException {
+        return getContent(it -> it < 0x80);
+    }
+
     public String getContent(Predicate<Integer> pred) throws IOException {
         try (BufferedInputStream input = new BufferedInputStream(new FileInputStream(file))) {
             StringBuilder output = new StringBuilder();
             int data;
-            while ((data = input.read()) > 0) {
+            while ((data = input.read()) != 0) {
                 if (pred.test(data)) {
                     output.append((char) data);
                 }
